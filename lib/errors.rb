ff32882731f18356f68a8aa754f477b1b6324352
Tanner Jones <jonesjoe@ecu.edu>
2013-11-11 13:38:33 -0500

    Adds FizzBuzz Errors and Starts using them.

---
+++ lib/errors.rb
@@ -0,0 +1,10 @@
+module FizzBuzz
+	
+	#FizzBuzz Errors
+	class FizzBuzzError < StandardError
+	end
+
+	#Raised when the initial value for FizzBuzz is greater than the last value
+	class InitializeError < FizzBuzzError
+	end
+end
