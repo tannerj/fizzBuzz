9f885bcc3ead2e4dc029e20c9c51412e761a8b8c
Tanner Jones <tannerjones10@gmail.com>
2014-05-09 15:09:57 -0400

    fixes spacing issues in lib/errors.rg

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,6 +1,6 @@
 module FizzBuzz
   #FizzBuzz Errors
-	class FizzBuzzError < StandardError
+  class FizzBuzzError < StandardError
 	end
 
   #Raised when the initial value for FizzBuzz is greater than the last value
