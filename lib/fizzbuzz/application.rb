c5cbdce29bc7a18c26357a8c79e9833c5132d084
Joel Tanner Jones <jonesjoe@ecu.edu>
2014-06-09 11:07:26 -0400

    Adds comments to FizzBuzz::Application.run and updates its range to -100..100

--- lib/fizzbuzz/application.rb
+++ lib/fizzbuzz/application.rb
@@ -7,9 +7,11 @@ module FizzBuzz
       @fizz_buzz = FizzBuzz.new
       self
     end
-    
+    #Application Runner, Creates a range of numbers to test
+    #For Fizz and Buzz and out puts the result to the command
+    #line.
     def run
-      (1..100).each do |number|
+      (-100..100).each do |number|
         fizz = @fizz_buzz.fizz(number)
         buzz = @fizz_buzz.buzz(number)
         if fizz && buzz then puts "fizzBuzz"
