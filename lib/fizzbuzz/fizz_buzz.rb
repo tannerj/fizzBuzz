11cdbeca12ccf0cd1f4d7cb5c7a44a843014dd8e
Joel Tanner Jones <jonesjoe@ecu.edu>
2014-06-25 15:55:13 -0400

    Adds comments to lib/fizzbuzz/fizz_buzz

--- lib/fizzbuzz/fizz_buzz.rb
+++ lib/fizzbuzz/fizz_buzz.rb
@@ -1,14 +1,17 @@
 module FizzBuzz
   class FizzBuzz
+    
+    #We fizz if the number being tested is evenly divisible by 3
     def fizz?(number)
-      if number == 0 then return false
+      if number == 0 then return false #Zero % any number will equal zero
       elsif number % 3 == 0 then return true
       else false 
       end
     end
-
+    
+    #We buzz if the number being tested is evenly divisible by 5
     def buzz?(number)
-      if number == 0 then return false
+      if number == 0 then return false #Zero % any number will equal zero
       elsif number % 5 == 0 then return true
       else false
       end
